a68a5183eadeb5b1e7ed4068f087b32117fd8186
unknown <sbarrera.96@hotmail.com>
2022-05-25 15:21:20 -0500

    Input values

--- Calculadora/src/main/java/com/sofkau/project/Main.java
+++ Calculadora/src/main/java/com/sofkau/project/Main.java
@@ -1,7 +1,21 @@
 package com.sofkau.project;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Logger logger = Logger.getLogger(Main.class.getName());
+
+        logger.info("Ingrese el primer número: ");
+        String valueA=scanner.nextLine();
+
+        logger.info("Ingrese el segundo número: ");
+        String valueB=scanner.nextLine();
+
+        logger.info("Ingrese operación que desee realizar: " +
+                "\n+: suma\n-: resta\n/: división\n*: multiplicación");
+        String operation=scanner.nextLine();
     }
 }
